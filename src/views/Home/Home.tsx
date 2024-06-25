87b962a592ba3d5aa32430bf5b771bd87e4918ff
andrewlora <jalc.andres.lora@gmail.com>
2024-06-24 23:33:21 -0700

    Add LastFiveDaysImages component into Home View

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -4,25 +4,45 @@ import Header from '../../components/Header';
 import getAPOD from '../../api/nasa';
 import TodaysImage from '../../components/TodaysImage';
 import {PostImage} from '../../types';
+import {format, sub} from 'date-fns';
+import LastFiveDaysImages from '../../components/LastFiveDaysImages/LastFiveDaysImages';
 const Home = () => {
   const [todaysImage, setTodaysImage] = useState<PostImage>({});
+  const [lastFiveDaysImages, setLastFiveDaysImages] = useState<PostImage[]>([]);
   useEffect(() => {
     loadTodaysImage();
+    loadLastFiveDaysImages();
   }, []);
 
   const loadTodaysImage = async () => {
     try {
-      const data = await getAPOD('&count=1');
-      setTodaysImage(data[0]);
+      const data = await getAPOD();
+      setTodaysImage(data);
     } catch (error) {
       console.error("Error loading today's image", error);
     }
   };
+
+  const loadLastFiveDaysImages = async () => {
+    try {
+      const date = new Date();
+      const todaysDate = format(date, 'yyyy-MM-dd');
+      const fiveDaysAgo = format(sub(date, {days: 5}), 'yyyy-MM-dd');
+      const data = await getAPOD(
+        `&start_date=${fiveDaysAgo}&end_date=${todaysDate}`,
+      );
+      setLastFiveDaysImages(data);
+    } catch (error) {
+      console.error('Error loading last 5 days images', error);
+    }
+  };
+
   const {container} = styles;
   return (
     <View style={container}>
       <Header />
       <TodaysImage {...todaysImage} />
+      <LastFiveDaysImages postImages={lastFiveDaysImages} />
     </View>
   );
 };
